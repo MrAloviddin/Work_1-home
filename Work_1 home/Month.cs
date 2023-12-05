5d8e3ae3728d31ffb185cd7401390baaff871494
user <user@WIN-02JVHUFFC8T>
2023-12-05 15:39:06 +0500

    project for month

---
+++ Work_1 home/Month.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_1_home
+{
+    enum  Month
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        Jule,
+        Aughust,
+        September,
+        October,
+        November,
+        December
+
+
+    }
+}
--- Work_1 home/Program.cs
+++ Work_1 home/Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using Work_1_home;
+
+string[] name = Enum.GetNames(typeof(Month));
+int[] number = (int[])Enum.GetValues(typeof(Month));
+
+Console.WriteLine("1)Tug'ulgan yil/oy/kun kiriting: ");
+Console.WriteLine("2)Hozirdan boshlab qolgan oylarni chiqarish");
+Console.WriteLine("3)Hozirgi oyning nomini olish");
+belgi:
+Console.Write("command: ");
+int command = Convert.ToInt32(Console.ReadLine());
+if (command == 1)
+{
+
+    Console.Write("yil/oy/kun : ");
+    string? birthData = Console.ReadLine();
+    string[]? data = birthData.Split('/');
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (Convert.ToInt32(data[1]) == number[i])
+        {
+            Console.WriteLine($"Siz tugulgan oy: {name[i]}");
+        }
+
+    }
+
+
+}
+if (command == 2)
+{
+    int nowMonth = DateTime.Now.Month;
+    for (int i = nowMonth; i <= 12; i++)
+    {
+        Console.WriteLine(Enum.GetName(typeof(Month), i));
+    }
+
+    Console.WriteLine();
+
+
+}
+if (command == 3)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (DateTime.Now.Month == number[i])
+        {
+            Console.WriteLine(name[i]);
+        }
+    }
+
+    Console.WriteLine();
+
+}
+goto belgi;
